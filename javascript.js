e36a1ca721e17722ded0556926d5a132b4cfb366
Serena Lew <36708701+minibaguettes@users.noreply.github.com>
2024-04-05 19:09:12 -0700

    minor fixes - added save

--- javascript.js
+++ javascript.js
@@ -2,6 +2,11 @@
 /* --- VARIABLES / ELEMENTS --- */
 /* ---------------------------- */
 
+const topTitle = document.getElementById('top-title');
+const topTitleText = document.createElement('p');
+topTitleText.textContent = 'untitled';
+topTitleText.setAttribute('id', 'top-title-text');
+topTitle.appendChild(topTitleText);
 const topTools = document.getElementById('top-tools');
 const botText = document.getElementById('bot-text');
 
@@ -70,26 +75,38 @@ newGridBtn.addEventListener("click", function() {
 // main tools - hover and display text on bottom
 document.addEventListener("mouseover", (e) => {
   let el = e.target;
+  let text = '';
   if (el.id == 'paint') {
-    botText.textContent = 'paint';
+    text = 'paint';
   }
   else if (el.id == 'etch-sketch') {
-    botText.textContent = 'etch sketch';
+    text = 'etch sketch';
   }
   else if (el.id == 'eraser') {
-    botText.textContent = 'eraser';
+    text = 'eraser';
   }
   else if (el.id == 'clear') {
-    botText.textContent = 'clear';
+    text = 'clear';
   }
-  else {
-    botText.textContent = 'for help, click help';
+  else if (el.id == 'outline') {
+    text = 'outline';
   }
+  else if (el.id == 'color-picker') {
+    text = 'pick a new color';
+  }
+  else if (el.id == 'random-color') {
+    text = 'random color';
+  }
+  else if (el.id.includes('#', 0)) {
+    text = el.id;
+  }
+  botText.textContent = text;
 });
 
 // main tools - click to select
 document.addEventListener("click", (e) => {
   let el = e.target;
+  // paint, eraser
   if (el.id == 'paint' || el.id == 'eraser') {
     // enable paint click
     gridContainer.addEventListener("mousedown", paintClick);
@@ -100,6 +117,7 @@ document.addEventListener("click", (e) => {
     }
     mode = el.id;
   }
+  // etch sketch brush
   else if (el.id == 'etch-sketch') {
     // disable paint click
     gridContainer.removeEventListener("mousedown", paintClick);
@@ -109,18 +127,24 @@ document.addEventListener("click", (e) => {
       gridContainer.childNodes[i].addEventListener("mouseover", paintDrag);
     }
   }
+  // clear screen
   else if (el.id == 'clear') {
     for (var i = 0; i < gridContainer.childNodes.length; i++) {
       gridContainer.childNodes[i].style.backgroundColor = '#FFFFFF';
     }
   }
+  // toggle grid outline
+  // when sending html to canvas via html2canvas, the container must have background color in order to see grid
+  //  so add/remove outline class to/from squares AND add/remove background color class to/from gridContainer
   else if (el.id == 'outline') {
     if (gridContainer.childNodes[0].classList.contains('outline')) {
+      gridContainer.classList.remove('grid-color');
       for (var i = 0; i < gridContainer.childNodes.length; i++) {
         gridContainer.childNodes[i].classList.remove('outline');
       }
     }
     else {
+      gridContainer.classList.add('grid-color');
       for (var i = 0; i < gridContainer.childNodes.length; i++) {
         gridContainer.childNodes[i].classList.add('outline');
       }
@@ -231,13 +255,15 @@ function paintDrag() {
 function shiftColorHistory() {
   if (currColor != lastColor) {                                         // if color has changed
     if (colorTools.querySelectorAll('.white').length == 0) {            // if all color history elements have been changed (already cycled through the queue),
-      for (var i = 5; i > 0; i--) {         //  then in descending order, replace the preceding element's color
+      for (var i = 5; i > 0; i--) {         //  then in descending order, replace the preceding element's color and id
         colorTools.children[i].style.backgroundColor = colorTools.children[i-1].style.backgroundColor;
+        colorTools.children[i].setAttribute('id', colorTools.children[i-1].getAttribute('id'));
       }
       colorIndex = 0;
     }
     colorTools.children[colorIndex].style.backgroundColor = currColor;  // set next color history 
     colorTools.children[colorIndex].classList.remove('white');          // remove default class; when all elements no longer have this class, it will return to front of queue
+    colorTools.children[colorIndex].setAttribute('id', currColor);      // set id to color hex
     colorIndex++;
     lastColor = currColor;
   }
@@ -257,3 +283,14 @@ function randomColor(e) {
 //     event.target.style.opacity =  getComputedStyle(event.target).opacity - 0.1;
 //   }
 // }
+
+// https://html2canvas.hertzen.com/
+// https://stackoverflow.com/questions/10673122/how-to-save-canvas-as-an-image-with-canvas-todataurl
+function takeScreenShot() {
+  html2canvas(document.querySelector(".capture")).then(canvas => {
+    // let newWin = window.open();
+    // newWin.document.body.appendChild(canvas);
+    var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // if you dont replace you will get a DOM 18 exception.
+    window.location.href=image; // save image locally
+  });
+}
